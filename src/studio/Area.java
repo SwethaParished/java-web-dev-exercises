009e2cbc3141a6d33e4d901231ad6a10f48a631a
SwethaParished <swethaparished@gmail.com>
2023-08-21 20:45:44 -0500

    Bonus mission of whileis added in area.java

--- src/studio/Area.java
+++ src/studio/Area.java
@@ -7,11 +7,13 @@ public class Area {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the radius:");
         double radius = input.nextDouble();
+        while(radius <= 0){
+            System.out.println("Enter the radius:");
+            radius = input.nextDouble();
+        }
         if(radius > 0) {
             double area = Circle.getArea(radius);
             System.out.println("Area of the Rectangle of radius " + radius + " is: " + area);
-        }else{
-            System.out.println("Enter the valid input");
         }
     }
 }
