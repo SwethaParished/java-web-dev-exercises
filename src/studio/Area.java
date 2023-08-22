2b6a1687956b0d1a0d7e3da5886367f79ec64d1f
SwethaParished <swethaparished@gmail.com>
2023-08-21 20:31:32 -0500

    completed Area and Circle

--- src/studio/Area.java
+++ src/studio/Area.java
@@ -1,2 +1,13 @@
-package studio;public class Area {
+package studio;
+
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the radius:");
+        double radius = input.nextDouble();
+        double area = 3.14 *
+        System.out.println("Area of the Rectangle:" + (length * length));
+    }
 }
